6f9cb4156b1455b4f851e820dd23fca5b4be8226
lil-armstrong <oebong1@gmail.com>
2023-04-25 14:01:39 +0100

    add all tasks

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+
+  package { 'python3-pip':
+    ensure => installed
+  }
+
+  exec { 'install_flask_with_pip3':
+    command => 'pip3 install flask==2.1.0',
+    path    => ['/usr/bin', '/usr/sbin',],
+    unless  => 'pip3 show flask | grep "Version: 2.1.0"',
+    require => Package['python3-pip']
+  }
