201095e796a48d15fcc6842f1cae1b8ec3ead5c4
lil-armstrong <oebong1@gmail.com>
2023-05-15 16:11:13 +0100

    refactor

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,5 +3,5 @@
 
 exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "/^\s*server_name\s*_;/a \\\n\tlocation /redirect_me {\n\t\treturn 301 http://tired.com;\n\t}\n" "/etc/nginx/sites-available/default" ; sudo service nginx start',
 }
