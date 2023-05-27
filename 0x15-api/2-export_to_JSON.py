7fad7c39ae01ba21a1b2e919e64a386876b42678
lil-armstrong <oebong1@gmail.com>
2023-05-27 03:05:11 +0100

    refactor

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,31 @@
- 
+#!/usr/bin/python3
+"""Get information about a given employee TODO list"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+
+    if len(argv) != 1:
+        print("Usage: %s <ID>" % (sys.argv[0]))
+    else:
+        try:
+            employee_id = argv[0]
+            api = "https://jsonplaceholder.typicode.com/"
+            file_name = "%s.json" % (employee_id)
+            user = requests.get(api + "users/%s" % (employee_id)).json()
+            todos = requests.get(
+                api + "todos", params={'userId': employee_id}).json()
+
+            rows = [{"task": t.get('title'),
+                     "completed": t.get('completed'),
+                     "username": user.get('username'),
+                     }
+                    for t in todos]
+
+            with open(file_name, 'w') as f:
+                json.dump({user.get('id'): rows}, f)
+        except Exception as err:
+            print(err)
