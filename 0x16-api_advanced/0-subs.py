54266c92965636eec208cfaa7ef744cf277783ba
lil-armstrong <oebong1@gmail.com>
2023-06-15 10:09:50 +0100

    refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""
-Script queries the Reddit API and returns the number \
-    of subscribers (not active users, total subscribers) for a given subreddit.
-"""
+""" a function that queries the Reddit API and returns the number of subscrib
+ers for a given subreddit."""
 import requests
 
-if __name__ == "__main__":
-    def get_subreddit_subscribers(subreddit):
-        url = f"https://www.reddit.com/r/{subreddit}/about.json"
-        # Set a custom User-Agent header
-        headers = {'User-Agent': 'Custom User Agent'}
 
-        try:
-            response = requests.get(url, headers=headers)
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        except (requests.exceptions.RequestException, KeyError):
-            return 0
+def number_of_subscribers(subreddit):
+    """Returnd the total no of subscribers in a subraddit"""
+    URL = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-agent": "linux:0x016.api.advanced:v1.0.0"
+    }
+    res = requests.get(URL, headers=headers, allow_redirects=False)
+    results = res.json().get("data", None)
+    if results is None:
+        return 0
+    return results.get("subscribers", 0)
