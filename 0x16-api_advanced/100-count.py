8e7281137c8680ba5ad621dffc9ce5b3313872de
lil-armstrong <okposong.ebong@celus.io>
2023-10-13 22:31:06 +0200

    refactor: 100-count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,55 +1,68 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
+"""
+Sub-reddit keyword frequency module
+
+Query the Reddit API for hot articles,
+Parse the title and
+Print a sorted count of given keywords (case-insensitive, delimited by spaces)
+"""
+
 import requests
+from typing import List
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+def count_words(subreddit: str, word_list: List[str], log: dict | None = None,
+                after: str | None = None, count: int | None = None) -> str:
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+Recursive fn that return the sorted count of given keywords
+Results are printed in descending order, by the count
+Words are printed in lowercase
+Results are based on the number of times a keyword appears,
+not titles it appears in
+    """
+    if log is not None and after is None:
+        entries = list(log.items())
+        sorted_by_count = sorted(entries, key=lambda x: (-x[1], x[0]))
+        for entry in sorted_by_count:
+            print(f"{entry[0]}: {entry[1]}")
+        return None
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-agent": "linux:0x016.api.advanced:v1.0.0"
     }
     params = {
+        "limit": 100,
         "after": after,
-        "count": count,
-        "limit": 100
+        "count": count
     }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
+    deduplicated = word_list
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
+    if log is None:
+        log = {}
+        lowercased = [word.lower() for word in word_list]
+        deduplicated = list(set(lowercased))
 
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
-    else:
-        count_words(subreddit, word_list, instances, after, count)
+    res = requests.get(url, headers=headers, params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
+        return None
+
+    result = res.json().get("data")
+    children = result.get("children")
+    after = result.get('after')
+    count = result.get('dist')
+
+    for data in children:
+        title: str = data.get("data").get("title")
+        title = title.lower()
+        words = title.split()
+
+        for keyword in deduplicated:
+            for word in words:
+                if word == keyword:
+                    log[keyword] = log.get(keyword, 0) + 1
+
+    
+    return count_words(subreddit, deduplicated, log, after, count)
