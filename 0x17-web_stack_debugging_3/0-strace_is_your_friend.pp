f400a26ab817f0a0ba1c3e7238f32a35d7823a9d
lil-armstrong <okposong.ebong@celus.io>
2023-10-13 21:37:46 +0200

    refactor: mod script

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,36 +1,6 @@
 # Script fixes Apache missing index.html file
 
-file { '/var/www/html/index.html':
-  ensure  => 'file',
-  content => '
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Holberton &#8211; Just another WordPress site</title>
-      <link rel="alternate" 
-      type="application/rss+xml" 
-      title="Holberton &raquo; Feed" 
-      href="http://127.0.0.1/?feed=rss2" />
-      <link rel="alternate" 
-      type="application/rss+xml" 
-      title="Holberton &raquo; Comments Feed" 
-      href="http://127.0.0.1/?feed=comments-rss2" />
-    </head>
-    <body>
-      <h1 class="site-title">
-        <a href="http://127.0.0.1/" rel="home">Holberton</a>
-      </h1>
-      <p>Yet another bug by a Holberton student</p>
-      <div>
-        <div id="wp-custom-header" 
-        class="wp-custom-header">
-          <img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" 
-          width="2000" 
-          height="1200" 
-          alt="Holberton" />
-        </div>
-      </div>
-    </body>
-    </html>
-  ',
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
